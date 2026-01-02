1c3bceb15da77d21f660f228cd37733b692e51d3
Kunal Bhalerao <kunalbhalerao2004@gmail.com>
2026-01-02 16:37:19 +0530

    Selenium Script

---
+++ src/locator/CssLocator.java
@@ -0,0 +1,26 @@
+package locator;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CssLocator {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		ChromeDriver driver = new ChromeDriver() ;
+		
+		driver.manage().window().maximize(); 
+		
+		driver.get("https://demowebshop.tricentis.com/");
+		
+		Thread.sleep(2000) ;
+		
+		driver.findElement(By.id("small-searchterms")).sendKeys("Cake") ;
+		
+		driver.findElement(By.cssSelector("input[value='Search']")).click() ;
+		
+		driver.close() ;
+		
+		
+	}
+}
