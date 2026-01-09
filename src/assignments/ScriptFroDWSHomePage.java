82577ed65ae401538cb83b0e6fb403a5f2f89b46
Kunal Bhalerao <kunalbhalerao2004@gmail.com>
2026-01-09 11:57:15 +0530

    Try and Error for script like base understaing

---
+++ src/assignments/ScriptFroDWSHomePage.java
@@ -0,0 +1,52 @@
+package assignments;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ScriptFroDWSHomePage {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		String exp_Url ="https://demowebshop.tricentis.com/" ;
+		
+		ChromeDriver driver = new ChromeDriver() ;
+		
+		driver.manage().window().maximize(); 
+		
+		driver.get("https://demowebshop.tricentis.com/");
+		
+		String act_Url= driver.getCurrentUrl() ;
+		
+		if (exp_Url.equals(act_Url)) {
+			
+			System.out.println("I am in DWS home Page and the test case is passed"); 
+			
+			List<WebElement> socialLinks = driver.findElements(By.xpath("//div[@class='column follow-us']/ul/li/a")) ;
+			
+			
+			for (WebElement webElement : socialLinks) {
+				
+				Thread.sleep(2000) ;
+				
+				String act_Url1 = driver.getCurrentUrl();
+				
+				if (act_Url1.equals(exp_Url)) {
+					
+					Thread.sleep(2000) ;
+					
+					webElement.click() ;
+				}
+				else {
+					driver.navigate().back() ;
+				}
+			}
+			
+		} else {
+			System.out.println("I am not in DWS home page and the testCase is fail");
+		}
+		
+	}
+}
