dd6aa86dc1d4863d2ff81c9b7823d7df14361f93
Kunal Bhalerao <kunalbhalerao2004@gmail.com>
2026-01-15 17:49:35 +0530

    Reading the text after sucessfuly logged in

---
+++ src/assignments/ReadTheMail.java
@@ -0,0 +1,12 @@
+package assignments;
+import  utilityClass.*;
+public class ReadTheMail {
+
+	public static void main(String[] args) {
+		
+		BaseClass b1 = new BaseClass() ;
+		b1.postConditon();
+		b1.login();
+		String email = 
+	}
+}
