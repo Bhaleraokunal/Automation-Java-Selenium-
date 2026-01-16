170b6af84893681b7f17562e6f742e3216a68521
Kunal Bhalerao <kunalbhalerao2004@gmail.com>
2026-01-16 11:44:25 +0530

    Update the utility base class

--- src/utilityClass/BaseClass.java
+++ src/utilityClass/BaseClass.java
@@ -5,28 +5,29 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseClass {
 
-	static ChromeDriver driver = new ChromeDriver() ;
+	public static ChromeDriver driver ;
 	
-	public static void preCondition( ) {
+	public static  void preCondition( ) {
 		
+		driver = new ChromeDriver() ;
 		driver.manage().window().maximize(); 
 		
 		driver.get("https://demowebshop.tricentis.com/");
 		
 	}
 	
-	public static void login() {
+	public static  void login() {
 		driver.findElement(By.className("ico-login")).click() ;
 		driver.findElement(By.id("Email")).sendKeys("kunalbhalerao2004@gmail.com"); 
 		driver.findElement(By.name("Password")).sendKeys("123456");
 		driver.findElement(By.cssSelector(".button-1.login-button")).click();
 	}
 	
-	public static void logout() {
+	public static  void logout() {
 		driver.findElement(By.linkText("Log out")).click(); 
 	}
 	
-	public static void postConditon() {
+	public static  void postConditon() {
 		driver.close(); 
 	}
 }
