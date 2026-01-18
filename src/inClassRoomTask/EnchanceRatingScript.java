778f87b79293ad432989238e4e3429df8644bef4
Kunal Bhalerao <kunalbhalerao2004@gmail.com>
2026-01-18 17:03:15 +0530

    Write a script for Qspider demo app to hover over a rating section

---
+++ src/inClassRoomTask/EnchanceRatingScript.java
@@ -0,0 +1,49 @@
+package inClassRoomTask;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class EnchanceRatingScript {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		ChromeDriver driver = new ChromeDriver() ;
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://demoapps.qspiders.com/");
+		
+		Actions act = new Actions(driver) ; 
+		
+		Thread.sleep(1000) ;
+		
+		driver.findElement(By.xpath("//a[@class='block w-[100%] h-full']")).click() ;
+		Thread.sleep(1000) ;
+
+		
+		driver.findElement(By.xpath("(//section[@class='poppins text-[15px]'])[8]")).click() ;
+		Thread.sleep(1000) ;
+
+		
+		driver.findElement(By.xpath("(//section[@class='poppins text-[14px]'])[2]")).click() ;
+		Thread.sleep(1000) ;
+
+		
+		driver.findElement(By.linkText("Ratings")).click() ;
+		Thread.sleep(1000) ;
+		
+		 List<WebElement> stars = driver.findElements(By.xpath("//div[@class='flex justify-center pt-2']//label")) ;
+		 
+		 System.out.println(stars); 
+		 
+		 for (WebElement webElement : stars) {
+			act.moveToElement(webElement).perform() ;
+		}
+		 
+		 driver.quit() ;
+	}
+}
