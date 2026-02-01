4722add204155951a4b27f770a64882105024f73
Kunal Bhalerao <kunalbhalerao2004@gmail.com>
2026-02-01 18:24:31 +0530

    Assignment given by selenium trainer

---
+++ src/assignments/AgodaAssignmenent.java
@@ -0,0 +1,12 @@
+package assignments;
+import allFactoryMethods.AgodaBaseTest; 
+
+public class AgodaAssignmenent  {
+
+	public static void main(String[] args) throws InterruptedException {
+	AgodaBaseTest.preCondtion();
+	AgodaBaseTest.departureDate();
+	AgodaBaseTest.returnDate();
+	AgodaBaseTest.postCondition(); 
+	}
+}
