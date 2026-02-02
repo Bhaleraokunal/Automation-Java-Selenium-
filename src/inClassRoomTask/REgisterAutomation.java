8cd897f0ed335229dd167b588bab57c9457c4786
Kunal Bhalerao <kunalbhalerao2004@gmail.com>
2026-02-02 22:25:10 +0530

    Automating the registration form

---
+++ src/inClassRoomTask/REgisterAutomation.java
@@ -0,0 +1,19 @@
+package inClassRoomTask;
+import allFactoryMethods.*;
+
+public class REgisterAutomation {
+
+	public static void main(String[] args) {
+		
+		DemoAutomationtestingIn.preCondtion(); 
+		DemoAutomationtestingIn.photo(); 
+		DemoAutomationtestingIn.names(); 
+		DemoAutomationtestingIn.details();
+		DemoAutomationtestingIn.hobbies();
+		DemoAutomationtestingIn.Others();
+		DemoAutomationtestingIn.dob();
+		DemoAutomationtestingIn.password();
+		DemoAutomationtestingIn.submit();
+		DemoAutomationtestingIn.postCondition();
+	}
+}
