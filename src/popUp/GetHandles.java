47859c995b03fd54d37ad118d5cd893f71dbb0fd
Kunal Bhalerao <kunalbhalerao2004@gmail.com>
2026-02-16 00:48:56 +0530

    Learning how to use the windowHandel methods

---
+++ src/popUp/GetHandles.java
@@ -0,0 +1,35 @@
+package popUp;
+
+import java.time.Duration;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class GetHandles {
+
+public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver() ;
+		driver.manage().window().maximize(); 
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15)) ;
+		driver.get("https://demowebshop.tricentis.com/");
+		String parent = driver.getWindowHandle() ;
+		System.out.println(parent);
+		Actions act = new Actions(driver) ;
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30)) ;
+		act.keyDown(Keys.PAGE_DOWN).keyDown(Keys.PAGE_DOWN).perform() ;
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Facebook']"))) ;
+		driver.findElement(By.xpath("//a[text()='Facebook']")).click() ;
+		Set<String> multi = driver.getWindowHandles() ;
+		
+		for (String win : multi) {
+			System.out.println(win);
+		}
+		}
+}
