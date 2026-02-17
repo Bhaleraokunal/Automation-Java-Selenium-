b67fff933f00283a2884873a412268244ff7af0b
Kunal Bhalerao <kunalbhalerao2004@gmail.com>
2026-02-18 00:14:30 +0530

    How to Automate a WebBrwoser

---
+++ src/inClassRoomTask/OpenBrowser.java
@@ -0,0 +1,10 @@
+package inClassRoomTask;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenBrowser {
+
+	public static void main(String[] args) {
+		ChromeDriver driver = new ChromeDriver() ;
+	}
+}
