c84615feac152378025e053e3292f874f4a759da
Kunal Bhalerao <kunalbhalerao2004@gmail.com>
2026-04-02 08:41:41 +0530

    Assignment for the Child Browser Using the WindowHandles

---
+++ src/assignments/ChildBrowserAssignment.java
@@ -0,0 +1,15 @@
+package assignments;
+
+public class ChildBrowserAssignment {
+
+	public static void main(String[] args) {
+		
+		ChildBrowserMethodsAssignment.preCondition();
+		ChildBrowserMethodsAssignment.facebook();
+		ChildBrowserMethodsAssignment.twitter();
+		ChildBrowserMethodsAssignment.youtube();
+		ChildBrowserMethodsAssignment.googel();
+		ChildBrowserMethodsAssignment.takeScreenshot();
+		ChildBrowserMethodsAssignment.postCondition();
+	}
+}
